8278981b3622926058a1caf0ca320803e7bf9b8b
Subhransu De <subhransu.de.official@gmail.com>
2025-04-28 23:16:25 +0530

    Add JsonInclude

---
+++ src/main/java/dev/subhransu/fasterxmljacksonexamples/JsonInclude/JsonIncludeExample.java
@@ -0,0 +1,16 @@
+package dev.subhransu.fasterxmljacksonexamples.JsonInclude;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+record JsonIncludeExample(String nullField, String notNullField) {}
+
+@RestController
+class JsonIncludeExampleController {
+  @GetMapping("/jsonInclude")
+  public JsonIncludeExample getJsonIncludeExample() {
+    return new JsonIncludeExample(null, "Not Null Field");
+  }
+}
