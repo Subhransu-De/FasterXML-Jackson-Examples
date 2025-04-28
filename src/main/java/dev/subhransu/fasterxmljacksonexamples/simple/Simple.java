c56eb52d9c730eb9b79240c1f6c7c2d779fe6359
Subhransu De <subhransu.de.official@gmail.com>
2025-04-28 23:16:03 +0530

    Add Simple

---
+++ src/main/java/dev/subhransu/fasterxmljacksonexamples/simple/Simple.java
@@ -0,0 +1,13 @@
+package dev.subhransu.fasterxmljacksonexamples.simple;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Simple {
+  @GetMapping("/hello")
+  public Map<String, String> helloWorld() {
+    return Map.of("message", "Hello, world!");
+  }
+}
