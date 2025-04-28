d1efc393aa4b8e0635c8ca81c3aef5d1d2fc1226
Subhransu De <subhransu.de.official@gmail.com>
2025-04-28 23:15:29 +0530

    Init

---
+++ src/main/java/dev/subhransu/fasterxmljacksonexamples/FasterXMLJacksonExamplesApplication.java
@@ -0,0 +1,12 @@
+package dev.subhransu.fasterxmljacksonexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasterXMLJacksonExamplesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FasterXMLJacksonExamplesApplication.class, args);
+  }
+}
---
+++ src/test/java/dev/subhransu/fasterxmljacksonexamples/FasterxmljacksonexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.subhransu.fasterxmljacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasterxmljacksonexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
