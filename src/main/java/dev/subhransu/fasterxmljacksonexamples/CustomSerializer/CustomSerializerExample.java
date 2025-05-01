b4c7e2f6f3adf04348709c46b069ba9eff5c329e
Subhransu De <subhransu.de.official@gmail.com>
2025-05-01 17:56:54 +0530

    Add JsonSerialize

---
+++ src/main/java/dev/subhransu/fasterxmljacksonexamples/CustomSerializer/CustomSerializerExample.java
@@ -0,0 +1,35 @@
+package dev.subhransu.fasterxmljacksonexamples.CustomSerializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.io.IOException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+class GreetingSerializer extends JsonSerializer<JsonSerializeExample> {
+  @Override
+  public void serialize(
+      JsonSerializeExample value, JsonGenerator gen, SerializerProvider serializers)
+      throws IOException {
+    gen.writeStartObject();
+    gen.writeFieldName("greetings");
+    gen.writeString("Hello, " + value.name());
+    gen.writeEndObject();
+  }
+}
+
+@JsonSerialize(using = GreetingSerializer.class)
+record JsonSerializeExample(String name) {}
+
+@RestController
+class CustomSerializerExampleController {
+
+  @PostMapping("/json-serialize")
+  public JsonSerializeExample getJsonSerialize(
+      @RequestBody JsonSerializeExample jsonSerializeExample) throws Exception {
+    return jsonSerializeExample;
+  }
+}
---
+++ src/test/java/dev/subhransu/fasterxmljacksonexamples/CustomSerializer/JsonSerializeExampleTest.java
@@ -0,0 +1,37 @@
+package dev.subhransu.fasterxmljacksonexamples.CustomSerializer;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class JsonSerializeExampleTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @Autowired private ObjectMapper objectMapper;
+
+  @Test
+  void shouldSucceed() throws Exception {
+    String requestBody =
+        """
+         {
+           "name": "John"
+         }""";
+    mockMvc
+        .perform(
+            post("/json-serialize").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.greetings").value("Hello, John"));
+  }
+}
+
